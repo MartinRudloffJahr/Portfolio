57429674db1d87611a6a07de3e11409b6b4910bf
Martin Rudloff <m7rudloff@gmail.com>
2023-11-01 17:20:22 +0100

    Updated portfolio content 8

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const iconData = {
   // Example:
   "icon1": {
     title: "Title for Icon 1",
-    description: "Description for Icon 1",
+    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
     images: [
         'images/button1.png',
         'images/button1.png',
@@ -12,7 +12,7 @@ const iconData = {
 },
     "icon2": {
     title: "Title for Icon 2",
-    description: "Description for Icon 2",
+    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
     images: [
         'images/button2.png',
         'images/button2.png',
@@ -22,7 +22,8 @@ const iconData = {
 
   "icon3": {
     title: "Title for Icon 3",
-    description: "Description for Icon 3",
+    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+
     images: [
         'images/button3.png',
         'images/button3.png',
@@ -31,7 +32,8 @@ const iconData = {
   },
   "icon4": {
     title: "Title for Icon 4",
-    description: "Description for Icon 4",
+    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+
     images: [
         'images/button4.png',
         'images/button4.png',
@@ -41,14 +43,144 @@ const iconData = {
   },
   "icon5": {
     title: "Title for Icon 5",
-    description: "Description for Icon 5",
+    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+
     images: [
         'images/button1.png',
         'images/button2.png',
         'images/button3.png',
     ]
   },
-
+  "icon6": {
+    title: "Title for Icon 6",
+    description: "Description for Icon 6",
+    images: [
+        'images/button1.png',
+        'images/button2.png',
+        'images/button3.png',
+    ]
+  },
+  "icon7": {
+    title: "Title for Icon 7",
+    description: "Description for Icon 7",
+    images: [
+        'images/button1.png',
+        'images/button2.png',
+        'images/button3.png',
+    ]
+  },
+  "icon8": {
+    title: "Title for Icon 8",
+    description: "Description for Icon 8",
+    images: [
+        'images/button1.png',
+        'images/button2.png',
+        'images/button3.png',
+    ]
+  },
+  "icon9": {
+    title: "Title for Icon 9",
+    description: "Description for Icon 9",
+    images: [
+        'images/button1.png',
+        'images/button2.png',
+        'images/button3.png',
+    ]
+  },
+  "icon10": {
+    title: "Title for Icon 10",
+    description: "Description for Icon 10",
+    images: [
+        'images/button1.png',
+        'images/button2.png',
+        'images/button3.png',
+    ]
+  },
+  "icon11": {
+    title: "Title for Icon 11",
+    description: "Description for Icon 11",
+    images: [
+        'images/button1.png',
+        'images/button2.png',
+        'images/button3.png',
+    ]
+  },
+  "icon12": {
+    title: "Title for Icon 12",
+    description: "Description for Icon 12",
+    images: [
+        'images/button1.png',
+        'images/button2.png',
+        'images/button3.png',
+    ]
+  },
+  "icon13": {
+    title: "Title for Icon 13",
+    description: "Description for Icon 13",
+    images: [
+        'images/button1.png',
+        'images/button2.png',
+        'images/button3.png',
+    ]
+  },
+  "icon14": {
+    title: "Title for Icon 14",
+    description: "Description for Icon 14",
+    images: [
+        'images/button1.png',
+        'images/button2.png',
+        'images/button3.png',
+    ]
+  },
+  "icon15": {
+    title: "Title for Icon 15",
+    description: "Description for Icon 15",
+    images: [
+        'images/button1.png',
+        'images/button2.png',
+        'images/button3.png',
+    ]
+  },  "icon16": {
+    title: "Title for Icon 16",
+    description: "Description for Icon 16",
+    images: [
+        'images/button1.png',
+        'images/button2.png',
+        'images/button3.png',
+    ]
+  },  "icon17": {
+    title: "Title for Icon 17",
+    description: "Description for Icon 17",
+    images: [
+        'images/button1.png',
+        'images/button2.png',
+        'images/button3.png',
+    ]
+  },  "icon18": {
+    title: "Title for Icon 18",
+    description: "Description for Icon 18",
+    images: [
+        'images/button1.png',
+        'images/button2.png',
+        'images/button3.png',
+    ]
+  },  "icon19": {
+    title: "Title for Icon 19",
+    description: "Description for Icon 19",
+    images: [
+        'images/button1.png',
+        'images/button2.png',
+        'images/button3.png',
+    ]
+  },  "icon20": {
+    title: "Title for Icon 20",
+    description: "Description for Icon 20",
+    images: [
+        'images/button1.png',
+        'images/button2.png',
+        'images/button3.png',
+    ]
+  },
 
 
 
@@ -80,6 +212,30 @@ function changeLinkColor() {
 function revertLinkColor() {
     this.style.color = '#FFFFFF'; // Reverting to white
 }
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  function scrollToSection(event) {
+    event.preventDefault();
+    var sectionId = this.getAttribute('href');
+    var section = document.querySelector(sectionId);
+    var headerOffset = 100; // Height of your header
+    var sectionPosition = section.getBoundingClientRect().top + window.pageYOffset;
+    var offsetPosition = sectionPosition - headerOffset;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: 'smooth'
+    });
+  }
+
+  var navLinks = document.querySelectorAll('nav ul li a');
+  navLinks.forEach(function(link) {
+    link.addEventListener('click', scrollToSection);
+  });
+});
+
 
 
 
